b166157846c2097fda4b745166e23166f0416fab
moohilmy <hilmy7132@gmail.com>
2023-12-24 04:40:24 +0200

    see your ip in open web

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import IPInput from "./IPInput.jsx";
 import IPOutput from "./IPoutput.jsx";
 import "./header.css"
+import { useDispatch } from "react-redux";
+import { ipData } from "../redux/api/ipApi.js";
 const Header = () => {
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(ipData(""))
+    },[dispatch])
     return ( 
         <header className="main-section">
             <h1 className="logo-name">IP Address Tracker</h1>
